15dfca1e805f7193adb3eddabc5a575829499bb6
hosein <hsgry800@gmail.com>
2026-07-02 11:00:54 +0330

    fix: serve API/media via nginx reverse proxy (same-origin, relative /api/ URL)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,4 +23,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve uploaded media files in production (behind the nginx reverse proxy)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
